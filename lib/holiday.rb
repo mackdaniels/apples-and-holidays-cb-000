e32c3b098241c0badb088ca295d4b5af742564ee
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-26 02:22:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
-    puts "#{season.capitalize}: "
+    puts "#{season.capitalize}:"
       holidays.each do |holiday, supplies|
         holiday = holiday.to_s
         if holiday ==  "new_years"
